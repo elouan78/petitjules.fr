f0f84db1d3d5fa3bb160f3ac2b5f027aedaaa51c
Elouan <elouanmace.professionnel@gmail.com>
2020-04-04 00:06:20 +0200

    panel configuration - mises en place projet wamp et ajout page php

---
+++ panel/panel.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <title>Panel configuration - Petitjules.fr</title>
+        <!--     style     -->
+        <link rel="stylesheet" href="../assets/css/styles.css">
+        <link rel="stylesheet" href="../assets/css/burger-menu.css">
+        <!-- FAVICON -->
+        <link rel="shortcut icon" type="ico" href="../assets/image/logo/settings.ico">
+    </head>
+
+    <body>
+        <header class="flex-row bg-couleur-officiel div">
+            <img src="../assets/image/logo/Logo-petitjules2.png" target="logo"width="95" height="95"/>
+
+            <div class="btn-navigation">
+                <div class="barre"></div>
+                <div class="barre"></div>
+                <div class="barre"></div>
+            </div>
+
+            <div class="navigation">
+                <ul>
+                    <li>
+                        <a href="">Mes travaux</a>
+                    </li>
+
+                    <li>
+                        <a href="">A propos</a>
+                    </li>
+
+                    <li>
+                        <a href="">Contact</a>
+                    </li>
+                </ul>
+            </div>
+
+        </header>
+
+        <main>
+            
+            <section class="flex-row">
+
+                <div class="bg-gray desk10">
+
+                <div class="bg-white desk1"></div>
+
+                </div>
+
+
+                
+            </section>
+
+
+
+
+            <!-- <section class="flex-row">
+                <div class="desk2 bg-red"></div>
+                <div class="desk2 bg-blue"></div>
+            </section>
+            <section class="flex-row">
+                <div class="desk4 bg-pink"></div>
+                <div class="desk4 bg-orange"></div>
+                <div class="desk4 bg-yellow"></div>
+                <div class="desk4 bg-brown"></div>
+            </section> -->
+        </main>
+
+        <footer class="bg-couleur-officiel ">
+
+            <h1>Marque de Vêtements</h1>
+
+            <h2>Email : contact@petitjules.fr</h2>
+            <h2>Réseaux sociaux :</2>
+
+                <a href="https://www.instagram.com/petitjules_/" target="_blank">Instagram<img src="../assets/image/social/instagram.png" width="45" height="45"></a>
+                <a href="https://twitter.com/petitjules_" target="_blank">Twitter<img src="../assets/image/social/twitter.png" width="45" height="45"></a>
+                <a href="https://dribbble.com/petitjules_" target="_blank">Dribbble<img src="../assets/image/social/dribbble.png" width="45" height="45"></a>
+                <a href="https://www.behance.net/petitjules_" target="_blank">Behance<img src="../assets/image/social/behance.png" width="45" height="45"></a>
+
+        </footer>
+
+        <script src="assets/javascript/index.js"></script>
+
+    </body>
+</html>
+
